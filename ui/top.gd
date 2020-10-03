789d773a3d87ad5890a7d6ceed81e20f8e1cbfe3
qhoang85 <quang.hoang85@gmail.com>
2020-10-03 20:59:06 +0200

    UI

--- ui/top.gd
+++ ui/top.gd
@@ -1,13 +1,13 @@
-extends ColorRect
+extends Control
 
-onready var day = $Day
-onready var funds = $Funds
+onready var day = $Day/Day
+onready var funds = $Funds/Funds
 
 
 func _ready():
-	update_top_bar()
+	update_stats_bar()
 
 
-func update_top_bar():
+func update_stats_bar():
 	day.set_text("Day:" + str(player_stats.day))
 	funds.set_text("$" + str(player_stats.funds) + "/" + "$" + str(player_stats.goal))
