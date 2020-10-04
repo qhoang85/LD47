0025ba4c3bd5074686d4ab05235aeef3b671ad73
qhoang85 <quang.hoang85@gmail.com>
2020-10-04 12:52:52 +0200

    sss

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -6,7 +6,7 @@ onready var animation_player =$AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _unhandled_input(_event):
